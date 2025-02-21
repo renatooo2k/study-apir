3be732d704c694ab842c044bfb190e6036dfe24e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-20 21:36:17 -0300

    Primeiro commit

---
+++ src/test/java/com/github/renatooo2k/study_apir/StudyApirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.renatooo2k.study_apir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
